902b081e8e7627a0a9cd6abbcc703f7c78f99ca3
alaw22 <lawsonalex1999@gmail.com>
2026-01-31 17:52:21 -0600

    Update: Read in messages.txt in 8 byte increments and print out to console

--- main.go
+++ main.go
@@ -1,10 +1,41 @@
 package main
 
 import (
-		// "os"
+		"os"
+		"log"
 		"fmt"
+		"io"
+		"errors"
 )
 
+const inputFilePath = "messages.txt"
+
 func main(){
-	fmt.Println("I hope I get the job!")
+
+	// Create bytes slice 
+	b := make([]byte, 8, 8)
+
+	// Open messages file
+	f, err := os.Open(inputFilePath)
+	if err != nil {
+		log.Fatalf("could not open '%s': %s\n", inputFilePath, err)
+	}
+	defer f.Close()
+
+	for {
+
+		n, err := f.Read(b)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+
+			fmt.Printf("error: %s\n", err.Error())
+			break
+		}
+
+		str := string(b[:n])
+		fmt.Printf("read: %s\n",str)
+
+	}
 }
