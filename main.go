6e9d2a1953682d798eb48aa4fef6e402f86b1cd4
alaw22 <lawsonalex1999@gmail.com>
2026-02-14 22:09:55 -0600

    Update: created getLinesChannel() function that returns a string channel for the main() function to iterate over

--- main.go
+++ main.go
@@ -11,46 +11,67 @@ import (
 
 const inputFilePath = "messages.txt"
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	// Create channel
+	lines := make(chan string)
+	
+	go func(){
+		// Create bytes slice 
+		b := make([]byte, 8, 8)
+
+		// Close context managers and channels at return
+		defer f.Close()
+		defer close(lines)
+
+		currentLineContents := ""
+		for {
+	
+			n, err := f.Read(b)
+			if err != nil {
+	
+				if currentLineContents != "" {
+					lines <- currentLineContents
+					currentLineContents = ""
+				}
+	
+				if errors.Is(err, io.EOF) {
+					return
+				}
+	
+				fmt.Printf("error: %s\n", err.Error())
+				return
+			}
+	
+			str := string(b[:n])
+			parts := strings.Split(str,"\n")
+			for i := 0; i < len(parts)-1; i++ {
+				lines <- currentLineContents + parts[i]
+				currentLineContents = ""
+			}
+	
+			currentLineContents += parts[len(parts)-1]
+	
+		}
+	}()
+
+	return lines
+}
+
 func main(){
 
-	// Create bytes slice 
-	b := make([]byte, 8, 8)
+
 
 	// Open messages file
 	f, err := os.Open(inputFilePath)
 	if err != nil {
 		log.Fatalf("could not open '%s': %s\n", inputFilePath, err)
 	}
-	defer f.Close()
+	// defer f.Close()
 
-	currentLineContents := ""
-	for {
+	lines := getLinesChannel(f)
 
-		n, err := f.Read(b)
-		if err != nil {
-
-			if currentLineContents != "" {
-				fmt.Printf("read: %s\n", currentLineContents)
-				currentLineContents = ""
-			}
-
-			if errors.Is(err, io.EOF) {
-				break
-			}
-
-			fmt.Printf("error: %s\n", err.Error())
-			break
-		}
-
-		str := string(b[:n])
-		parts := strings.Split(str,"\n")
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s%s\n", currentLineContents, parts[i])
-			currentLineContents = ""
-		}
-
-		currentLineContents += parts[len(parts)-1]
-
+	for line := range lines{
+		fmt.Printf("read: %s\n",line)
 	}
 
 
