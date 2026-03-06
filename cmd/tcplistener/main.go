4df8380a4019b30f8de89749cc8b6125e7ced76b
alaw22 <lawsonalex1999@gmail.com>
2026-03-06 14:50:05 -0600

    Update: replaced getLinesChannel function in tcplistener with our RequestFromReader parser

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,71 +1,22 @@
 package main
 
 import (
-		// "os"
-		"log"
-		"fmt"
-		"io"
-		"errors"
-		"strings"
-		"net"
+	"fmt"
+	"log"
+	"net"
+
+	"http_protocol/internal/request"
 )
 
 const inputFilePath = "messages.txt"
 const port = ":42069"
 
-func getLinesChannel(conn io.ReadCloser) <-chan string {
-	// Create channel
-	lines := make(chan string)
-	
-	go func(){
-		// Create bytes slice 
-		b := make([]byte, 8, 8)
-
-		// Close context managers and channels at return
-		defer conn.Close()
-		defer close(lines)
-
-		currentLineContents := ""
-		for {
-	
-			n, err := conn.Read(b)
-			if err != nil {
-	
-				if currentLineContents != "" {
-					lines <- currentLineContents
-					currentLineContents = ""
-				}
-	
-				if errors.Is(err, io.EOF) {
-					return
-				}
-	
-				fmt.Printf("error: %s\n", err.Error())
-				return
-			}
-	
-			str := string(b[:n])
-			parts := strings.Split(str,"\n")
-			for i := 0; i < len(parts)-1; i++ {
-				lines <- currentLineContents + parts[i]
-				currentLineContents = ""
-			}
-	
-			currentLineContents += parts[len(parts)-1]
-	
-		}
-	}()
-
-	return lines
-}
-
-func main(){
-
+func main() {
 
 	// Open TCP Listener
-	listener, err := net.Listen("tcp",port)
-	if err != nil{
-		log.Fatalf("Could not establish TCP listener: %s\n",err.Error())
+	listener, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Could not establish TCP listener: %s\n", err.Error())
 	}
 	defer listener.Close()
 
@@ -74,22 +25,21 @@ func main(){
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Fatalf("Error in listener.Accept(): %s\n",err.Error())
+			log.Fatalf("Error in listener.Accept(): %s\n", err.Error())
 		}
 
 		fmt.Println("Accepted connection from", conn.RemoteAddr())
-		
-		lines := getLinesChannel(conn)
 
-		for line := range lines{
-			fmt.Println(line)
-		}
-		
-		fmt.Println("Connection to ", conn.RemoteAddr(),"closed")
+		req, err := request.RequestFromReader(conn)
+
+		fmt.Printf("Request line:\n - Method: %s\n - Target: %s\n - Version: %s\n",
+			req.RequestLine.Method,
+			req.RequestLine.RequestTarget,
+			req.RequestLine.HttpVersion)
+
+		fmt.Println("Connection to ", conn.RemoteAddr(), "closed")
 
 	}
 
-
-
-
 }
+
